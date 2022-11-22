960a835f90b112ad64f35b2aa9e742c33277fe0f
sakyAIU <114678581+sakyAIU@users.noreply.github.com>
2022-11-22 20:49:33 +0800

    C Notes

---
+++ 2_comments.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+
+{
+    //Comments-single line comments and multiple line comments
+    /*This is for multiple lines comments-
+    if you do the line break in single line comment quotation then it won't work correctly. It will show a bug there then. Hence, we have another types of comment tag that we use to comment multiple lines.
+    this is for this work.
+    */
+   
+   printf("Commetns!");
+   printf("single line comment"); //Single line comment
+   printf("Multiple Line Comment"); /*This is for
+   Multiple Line
+   Comments*/
+   return 0;
+}
---
+++ 3_variables.c
@@ -0,0 +1,36 @@
+#include<stdio.h>>
+int main()
+{
+    //LITERALS
+    
+    /*In programming, we will be dealing with different types of data.
+    All these fixed data (values) that we can use directly in our program are called literals. For example, 5, -23.5, 'c', etc.
+    Data types: 1. Integer Literals: 1, 2, 45. etc
+                2. Floating point literals: 24.323
+                3. Character Literals: A single character that is enclosed within single quotation marks. 's' , '8', '*', 'N'
+    */
+
+   //VARIABLES
+
+   /*Variables Declaration
+
+        Syntax: variable type variable name
+                    int number;
+                Here int is the variable type and number is the variable name. The number variable can only store the Integer values.
+
+   */
+  /*Assign values to variables
+                int age;
+                age=25;
+  */
+
+    /*Declare and assign value together
+                int age=25;
+    */
+
+
+   int num;
+   num=25;
+   printf("number: %d\n", num);
+   return 0;
+}
